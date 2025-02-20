e866026a23373ddb420470c3cdf7294d33b46d17
Mahatir Ahmed Tusher <mahatirtusher@gmail.com>
2025-02-21 01:22:55 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "Starting server on port $PORT"
-gunicorn --bind 0.0.0.0:$PORT --timeout 120 --workers 4 app:app
+echo "🔄 Starting server on port $PORT"
+exec gunicorn --bind 0.0.0.0:$PORT --timeout 120 --workers 4 app:app
